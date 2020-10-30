8d1a514d32ff0bfacf0c807af6b6c921d0d636fa
Gregory Bastianelli <greg@gerrg.com>
2020-10-30 11:57:52 -0400

    update relative api endpoint, setup for heroku

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
+
 let MONGODB_URI = process.env.MONGODB_URI;
 
 if (process.env.NODE_ENV === "test") {
